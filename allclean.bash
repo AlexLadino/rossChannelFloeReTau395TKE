240a0468ea9edfaa1d70142a7d2ab5429dd0cc62
AlexLadino <jair.ladino@correounivalle.edu.co>
2021-05-04 15:39:10 -0500

    allclean y modificado allrun

---
+++ allclean.bash
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd ${0%/*} || exit 1
+
+rm -r processor*
+rm log*
+rm -r postProcessing
--- allrun.bash
+++ allrun.bash
@@ -3,11 +3,9 @@ cd ${0%/*} || exit 1
 
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
-numProc=6
+numProc=4
 decomposePar
 
 # correr transient
-rm -r ./system # ok
-cp -r ./system.tke ./system # ok
-cp -r ./constant.tke/* ./constant/
+cp -r ./system/controlDict.tke ./system/controlDict # ok
 mpiexec -np $numProc pimpleTKEFoamV2021 -parallel
